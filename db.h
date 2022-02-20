1f0b6f172f80235925f38fbc574faf7ea2868dfc
Yar Sawyer <idquantum@gmail.com>
2022-02-21 02:01:43 +0300

    height testnet:mainnet

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 2000 : 1150000;
+    return testnet ? 0 : 0;
 }
 
 std::string static inline ToString(const CService &ip) {
